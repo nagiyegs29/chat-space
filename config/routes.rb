6946bc1bbbcb4f9b65ab184d103e27f7a481e57a
nagiyegs29 <nagi.yegs29@gmail.com>
2020-08-15 02:25:44 +0900

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,10 @@ Rails.application.routes.draw do
   root "groups#index"
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
-<<<<<<< Updated upstream
     resources :messages, onry: [:index, :create]
 
     namespace :api do
       resources :messages, only: :index, defaults: { format: "json"}
     end
-=======
-    resources :messages, only: [:index, :create]
->>>>>>> Stashed changes
   end
 end
